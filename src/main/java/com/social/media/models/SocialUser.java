e768dad06c177649816163f1b55fc3afb6e0eb96
Nikhil Singh <nikhilsingh20102002@gmail.com>
2025-04-26 16:28:29 +0530

    Implement bidirectional OneToOne mapping between SocialUser and SocialProfile entities

---
+++ src/main/java/com/social/media/models/SocialUser.java
@@ -0,0 +1,13 @@
+package com.social.media.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class SocialUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    @JoinColumn(name = "social_profile_id")
+    private SocialProfile socialProfile;
+}
